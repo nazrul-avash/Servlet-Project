25573b9d994194437c476edaaf0f959869101176
nazrul.avash@gmail.com <nazrul.avash@gmail.com>
2024-07-11 00:00:42 +0600

    logger

--- src/main/java/com/bazlur/eshoppers/web/HomeServlet.java
+++ src/main/java/com/bazlur/eshoppers/web/HomeServlet.java
@@ -4,6 +4,9 @@ import com.bazlur.dto.ProductDTO;
 import com.bazlur.repository.DummyProductRepositoryImpl;
 import com.bazlur.service.ProductService;
 import com.bazlur.service.ProductServiceImpl;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,9 +18,15 @@ import java.util.List;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeServlet.class);
+
     private ProductService productService = new ProductServiceImpl(new DummyProductRepositoryImpl());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("Logging on");
+        for(int i  = 0; i<1000;i++){
+           LOGGER.info("Dummy Log");
+        }
         List<ProductDTO> allProducts= productService.findAllProductSortedByName();
         req.setAttribute("products",allProducts);
         req.getRequestDispatcher("/WEB-INF/home.jsp").forward(req,resp);
